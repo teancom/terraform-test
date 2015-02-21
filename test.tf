ac4afe28750f6f7eac90cd3a699a03c93bb761d3
David Bishop <david@gnuconsulting.com>
2015-02-20 19:44:09 -0500

    broken source_dest_check

--- test.tf
+++ test.tf
@@ -4,27 +4,16 @@ provider "aws" {
 	region = "${var.aws_region}"
 }
 
-module "vpc" {
-  source = "github.com/cloudfoundry-community/terraform-aws-vpc"
-  network = "${var.network}"
-  aws_key_name = "${var.aws_key_name}"
-  aws_access_key = "${var.aws_access_key}"
-  aws_secret_key = "${var.aws_secret_key}"
-  aws_region = "${var.aws_region}"
-  aws_key_path = "${var.aws_key_path}"
-}
+resource "aws_instance" "test" {
+  ami = "ami-1d2b2958"
+  instance_type = "m3.medium"
+  key_name = "cf-sunrise"
+  associate_public_ip_address = true
+  security_groups = ["sg-f2f55397"]
+  subnet_id = "subnet-6e0ad40b"
+  source_dest_check = false
+  tags {
+   Name = "test server"
+  }
 
-module "cf" {
-  source = "github.com/cloudfoundry-community/terraform-aws-cf"
-  network = "${var.network}"
-  aws_key_name = "${var.aws_key_name}"
-  aws_access_key = "${var.aws_access_key}"
-  aws_secret_key = "${var.aws_secret_key}"
-  aws_region = "${var.aws_region}"
-  aws_key_path = "${var.aws_key_path}"
-  aws_vpc_id = "${module.vpc.aws_vpc_id}"
-  aws_internet_gateway_id = "${module.vpc.aws_internet_gateway_id}"
-  aws_route_table_public_id = "${module.vpc.aws_route_table_public_id}"
-  aws_route_table_private_id = "${module.vpc.aws_route_table_private_id}"
-  aws_subnet_lb_availability_zone = "${module.vpc.aws_subnet_bastion_availability_zone}"
 }
